6a1662478278563c0d19dde00deca23e0923bb6a
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-22 21:38:57 +0700

    up

--- select_from.sql
+++ select_from.sql
@@ -12,3 +12,26 @@ FROM [dbo].[Products];
 -- lấy tên công ty của khách hàng và quốc gia
 SELECT [CompanyName], [Country] -- SELECT CompanyName, Country
 FROM [dbo].[Customers]; -- FROM dbo.Customers
+
+-- bài tập
+-- Viết sql lấy tên công ty và số điện thoại của tất cả nhà CC
+SELECT CompanyName, Phone
+FROM dbo.Suppliers;
+
+--Lấy ra Category Name, Description và Picture trong bảng Categories
+SELECT [CategoryName],[Description],[Picture]
+FROM [dbo].[Categories]
+
+-- Lấy ra mã đơn hàng, CustomerID ở trong bảng Orders 
+SELECT [OrderID],[CustomerID]
+FROM [dbo].[Orders]
+
+--Lấy ra hết toàn bộ thông tin từ Shippers và Employees
+SELECT *
+FROM [dbo].[Shippers], [dbo].[Employees]
+
+--Lọc tên nhà cung cấp có đơn giá thấp nhất
+SELECT [ProductName], [UnitPrice]
+FROM [dbo].[Products]
+ORDER BY [UnitPrice] DESC
+
