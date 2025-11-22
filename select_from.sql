91e2777c06c3076fba98b9e7df79d205aea722e4
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-22 21:17:02 +0700

    UPDATE

--- select_from.sql
+++ select_from.sql
@@ -2,3 +2,8 @@
 USE [NORTHWND_NEW];
 SELECT [ProductName]
 FROM [dbo].[Products];
+
+-- viết câu lệnh lấy tên 
+-- giá bán mỗi đơn vị, số lượng mỗi đơn vị
+SELECT [ProductName], [UnitPrice],[QuantityPerUnit]
+FROM [dbo].[Products];
