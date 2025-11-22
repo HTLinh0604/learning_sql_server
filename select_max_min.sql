ed47f957426cb3cb7bb1603c83343d6bc5188fba
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-22 23:28:14 +0700

    up

--- select_max_min.sql
+++ select_max_min.sql
@@ -1,1 +1,18 @@
 use [NORTHWND_NEW];
+-- min of table products
+select min(p.UnitPrice) as "gia thap nhat"
+from Products as p;
+
+-- max day of order
+select max(o.OrderDate) as "ngay gan nhat"
+from Orders as o
+
+--find product , ID and name, max unitslnstock
+select max(p.UnitsInStock) as "so luong ton kho"
+from Products as p
+
+
+-- bai tap
+-- max ege employee
+select min(e.BirthDate) 
+from Employees as e 
