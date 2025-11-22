95de15fcae0a8c24a30daa4e43c22dc0ccf2e41f
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-22 23:12:14 +0700

    up

--- alias.sql
+++ alias.sql
@@ -1,1 +1,19 @@
 use [NORTHWND_NEW];
+-- lay CompanyName and mabuudien change name
+select  CustomerID as [makh], CompanyName as tencongty, PostalCode as "mabuudien", City "thanhpho"
+from Customers;
+
+
+--lastname and firstname change name
+select LastName as "Ho", FirstName as "Ten"
+from Employees
+
+-- top 15 of orders as o
+select top 15 o.*
+from Orders as "o"
+
+
+--bai tap
+--top 5 table products as p, productname as tensanpham, supplierID as manhacc, categoryIS as matheloai
+select top 5 p.ProductName as "tensanpham", p.SupplierID as [manhacc], p.CategoryID as matheloai
+from Products as "p"
