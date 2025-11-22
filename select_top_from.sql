319c926f5429bb2f628fd95e93bf13c63f7eed90
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-22 22:23:43 +0700

    up

--- select_top_from.sql
+++ select_top_from.sql
@@ -1,1 +1,17 @@
 use [NORTHWND_NEW];
+-- lay top 10 of customers
+select  top 10 *
+from Customers
+
+-- 30% of employee
+select top 30 percent *
+from Employees
+
+-- top 5 customersID , distinct
+select distinct top 5 CustomerID
+from Orders
+
+
+--top 3 product, distinct
+select distinct top 3 CategoryID
+from Products
