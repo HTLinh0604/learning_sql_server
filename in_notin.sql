90425e990a16766b63d6406ad577af40bf8daea1
Huynh Thai Linh <119388373+HTLinh0604@users.noreply.github.com>
2025-11-26 22:30:26 +0700

    up

--- in_notin.sql
+++ in_notin.sql
@@ -9,3 +9,25 @@ where o.ShipCountry in ('Germany','UK','Brazil');
 select o.ShipCountry
 from Orders as o 
 where o.ShipCountry not in ('Germany','UK','Brazil');
+
+-- lay ma cac sp co ma the loai <> 2,3,4
+select p.CategoryID
+from Products as p
+where p.CategoryID not in (2,3,4)
+
+--liet ke cac nv ko phai la nu tu bang nhan vien
+select e.TitleOfCourtesy
+from Employees as e
+where e.TitleOfCourtesy not in ('Ms.','Mrs.')
+
+--liet ke cac nv tu bang nhan vien
+select e.TitleOfCourtesy
+from Employees as e
+where e.TitleOfCourtesy  in ('Ms.','Mrs.')
+
+
+-- lay all khach hang den tu tp: Berlin, London, Warszawa
+select c.City
+from Customers as c
+
+where c.City in ('Berlin', 'London', 'Warszawa')
